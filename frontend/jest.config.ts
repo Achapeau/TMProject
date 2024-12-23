ca9c8467e8563a8d7bd1fa40472afac1a4d6b7be
Achapeau <127884427+Achapeau@users.noreply.github.com>
2024-12-23 14:34:25 +0100

    Configure Jest for unit testing with Next.js
    Added jest-environment-jsdom and set test environment to jsdom. Enabled automatic mock clearing and V8 coverage provider.

---
+++ frontend/jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "jest";
+import nextJest from "next/jest";
+
+const config: Config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+};
+
+export default config;
