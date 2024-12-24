d46fac8ed5ff5b37add5cf894611784d53165518
Achapeau <127884427+Achapeau@users.noreply.github.com>
2024-12-24 14:56:56 +0100

    Update Footer.unit.test.tsx . Having links for policy and terms is not certain, the other test was inappropriate

--- frontend/__tests__/Footer.unit.test.tsx
+++ frontend/__tests__/Footer.unit.test.tsx
@@ -2,26 +2,14 @@ import "@testing-library/jest-dom/";
 import { render, screen } from "@testing-library/react";
 import { Footer } from "@/_components/footer";
 
-const setup = () => {
-  render(<Footer />);
-  return {
-    policy: screen.getByText(/policy/i),
-    terms: screen.getByText(/terms/i),
-  };
-};
-
 describe("Footer", () => {
   it("should render successfully policy and terms links", () => {
-    const { policy, terms } = setup(); // ARRANGE & ACT
+    render(<Footer />); // ARRANGE
+
+    const policy = screen.getByText(/policy/i);
+    const terms = screen.getByText(/terms/i); // ACT
 
     expect(policy).toBeInTheDocument();
     expect(terms).toBeInTheDocument(); // ASSERT
-  });
-
-  it("links should have an href", () => {
-    const { policy, terms } = setup(); // ARRANGE & ACT
-
-    expect(policy).toHaveAttribute("href");
-    expect(terms).toHaveAttribute("href"); // ASSERT
   });
 });
