bc1009b53f5c71bac20976e71e0fc479b8afa6ff
Achapeau <127884427+Achapeau@users.noreply.github.com>
2025-01-29 11:51:05 +0100

    add "afterSignOutUrl" params to ClerkProvider to redirect to the homepage when you logout

--- frontend/app/(platform)/layout.tsx
+++ frontend/app/(platform)/layout.tsx
@@ -3,7 +3,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className='h-full bg-slate-100 flex items-center justify-center'>
-      <ClerkProvider>{children}</ClerkProvider>
+      <ClerkProvider afterSignOutUrl='/'>{children}</ClerkProvider>
     </div>
   );
 };
