a23a734f5b7d35d55fab97d10db9e92833230546
Achapeau <127884427+Achapeau@users.noreply.github.com>
2025-01-30 10:05:52 +0100

    install prisma to connect project to the database (MySQL) and create server actions for add and delete elements on the board

---
+++ frontend/app/(platform)/(dashboard)/organization/[organizationId]/_components/form.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { create } from "@/actions/create-board";
+import { useActionState } from "react";
+import { FormInput } from "./form-input";
+import { FormButton } from "./form-button";
+
+export const Form = () => {
+  const initialState = { message: "", errors: {} };
+  const [state, dispatch] = useActionState(create, initialState);
+  return (
+    <form action={dispatch}>
+      <div className='flex flex-col space-y-2'>
+        <FormInput errors={state?.errors} />
+      </div>
+      <FormButton />
+    </form>
+  );
+};
