82949752ceee396c03e7e48855391a07e467ebe9
Achapeau <127884427+Achapeau@users.noreply.github.com>
2025-02-01 23:33:41 +0100

    Create board and and navbar: -create server action for update and delete board, - Navbar to change board name, - Board options to delete board and redirect to organization

--- frontend/app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
+++ frontend/app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
@@ -1,18 +1,35 @@
 "use client";
 
+import { toast } from "sonner";
 import { useRef, useState } from "react";
+import { Board } from "@prisma/client";
 
 import { Button } from "@/components/ui/button";
-import { Board } from "@prisma/client";
 import { FormInput } from "@/components/form/form-input";
+import { updateBoard } from "@/actions/update-board";
+import { useAction } from "@/hooks/use-action";
+import { set } from "lodash";
 
 interface BoardTitleFormProps {
   data: Board;
 }
 
 export const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
+  const { execute } = useAction(updateBoard, {
+    onSuccess: (data) => {
+      toast.success(`Board "${data.title}" updated!`);
+      setTitle(data.title);
+      disableEditing();
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
   const formRef = useRef<HTMLFormElement>(null);
   const inputRef = useRef<HTMLInputElement>(null);
+
+  const [title, setTitle] = useState(data.title);
   const [isEditing, setIsEditing] = useState(false);
 
   const enableEditing = () => {
@@ -26,7 +43,12 @@ export const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
 
   const onSubmit = (formData: FormData) => {
     const title = formData.get("title") as string;
-    console.log("I have submitted", title);
+
+    execute({ title, id: data.id });
+  };
+
+  const onBlur = () => {
+    formRef.current?.requestSubmit();
   };
 
   if (isEditing) {
@@ -38,8 +60,8 @@ export const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
         <FormInput
           ref={inputRef}
           id='title'
-          onBlur={() => {}}
-          defaultValue={data.title}
+          onBlur={onBlur}
+          defaultValue={title}
           classname='text-lg font-bold px-[7px] py-1 h-7 bg-transparent focus-visible:outline-none focus-visible:ring-transparent border-none'
         />
       </form>
@@ -51,7 +73,7 @@ export const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
       onClick={enableEditing}
       className='font-bold text-lg h-auto w-auto p-1 px-2'
       variant='transparent'>
-      {data.title}
+      {title}
     </Button>
   );
 };
--- frontend/app/(platform)/(dashboard)/board/[boardId]/layout.tsx
+++ frontend/app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -1,29 +1,26 @@
-import { auth } from "@clerk/nextjs/server";
 import { notFound, redirect } from "next/navigation";
 
 import { db } from "@/lib/db";
-
+import { auth } from "@clerk/nextjs/server";
+import { title } from "process";
 import { BoardNavbar } from "./_components/board-navbar";
-
-const getBoard = async (boardId: string, orgId: string) => {
-  const board = await db.board.findUnique({
-    where: { id: boardId, orgId },
-  });
-
-  return board;
-};
 
 export async function generateMetadata({
   params,
 }: {
   params: { boardId: string };
 }) {
-  console.log("boardId", params.boardId);
   const { orgId } = await auth();
 
-  if (!orgId) return { title: "Board" };
+  if (!orgId) {
+    return {
+      title: "Board",
+    };
+  }
 
-  const board = await getBoard(params.boardId, orgId);
+  const board = await db.board.findUnique({
+    where: { id: params.boardId, orgId },
+  });
 
   return {
     title: board?.title || "Board",
@@ -37,22 +34,28 @@ const BoardIdLayout = async ({
   children: React.ReactNode;
   params: { boardId: string };
 }) => {
-  console.log("boardId", params.boardId);
   const { orgId } = await auth();
 
-  if (!orgId) return redirect("/select-org");
+  if (!orgId) {
+    redirect("/select-org");
+  }
 
-  const board = await getBoard(params.boardId, orgId);
+  const board = await db.board.findUnique({
+    where: { id: params.boardId, orgId },
+  });
 
-  if (!board) notFound();
+  if (!board) {
+    notFound();
+  }
 
   return (
     <div
-      className='relative h-full bg-no-repeat bg-center bg-cover'
-      style={{ backgroundImage: `url(${board.imageFullUrl})` }}>
-      <BoardNavbar data={board} />
-      <div className='absolute inset-0 bg-black/10' />
-      <main className='relative pt-28 h-full'>{children}</main>
+      style={{ backgroundImage: `url(${board.imageFullUrl})` }}
+      className='relative h-full bg-no-repeat bg-cover bg-center'>
+      <main className='relative pt28 h-full'>
+        <BoardNavbar data={board} />
+        {children}
+      </main>
     </div>
   );
 };
