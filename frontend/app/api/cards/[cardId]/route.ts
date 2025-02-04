7235014f8a0462154e5d1cb76f9836af843922ab
Achapeau <127884427+Achapeau@users.noreply.github.com>
2025-02-04 16:58:23 +0100

    Card actions: - Add a description, - Copy card, - Delete card, - create server actions

--- frontend/app/api/cards/[cardId]/route.ts
+++ frontend/app/api/cards/[cardId]/route.ts
@@ -2,10 +2,8 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-export async function GET(
-  req: Request,
-  { params }: { params: { cardId: string } }
-) {
+export async function GET(req: Request, props: { params: Promise<{ cardId: string }> }) {
+  const params = await props.params;
   try {
     const { userId, orgId } = await auth();
 
